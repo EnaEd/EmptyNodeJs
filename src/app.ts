27f736d90e1ec9bd6f836d778545a7cc029ef8a0
developer <stephencgregory@gmail.com>
2020-02-20 09:49:21 +0200

    add new node version & nodemon.json

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import 'dotenv/config';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+console.log(`test envir = ${process.env['TEST']} 323`);
 app.listen(3000, () =>
-  console.log(`test env= ${process.env['TEST']}`),
+  console.log(`test envir = ${process.env['TEST']} 32`),
 );
+
+console.log('test');
