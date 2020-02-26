ec1f4491386c7fec24c60e9ebf22c250afd32b22
developer <stephencgregory@gmail.com>
2020-02-26 12:03:24 +0200

    add koa-mount

--- server-api/src/app.ts
+++ server-api/src/app.ts
@@ -1,9 +1,22 @@
 import koa from 'koa';
+import koaRouter from 'koa-router';
+import mount from 'koa-mount';
+
+const home = new koa();
+home.use(async (ctx, next) => {
+  await next();
+  ctx.body = 'home page';
+});
+
+const version = new koa();
+version.use(async (ctx, next) => {
+  await next();
+  ctx.body = process.env.npm_package_version;
+});
 
 const app = new koa();
 
-app.use(async ctx => {
-  ctx.body = 'hello orld';
-});
+app.use(mount('/version', version));
+app.use(mount('/', home));
 
 app.listen(3000);
