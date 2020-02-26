2d67ae4989dee6008a13288bcc5df259790e20b7
developer <stephencgregory@gmail.com>
2020-02-26 10:06:36 +0200

    fix docker file

--- server-api/src/api/api.ts
+++ server-api/src/api/api.ts
@@ -1,5 +1,1 @@
-const version = () => process.env.npm_package_version;
 
-const home = () => 'hello world';
-
-export default { version, home };
--- server-api/src/app.ts
+++ server-api/src/app.ts
@@ -1,14 +1,9 @@
 import koa from 'koa';
-import router from 'koa-router';
-import api from './api/api';
-import mount from 'koa-mount';
 
 const app = new koa();
-const rout = new router();
 
-rout.get('/verson', api.version);
-rout.get('*', api.home);
-
-app.use(mount('', rout.middleware()));
+app.use(async ctx => {
+  ctx.body = 'hello orld';
+});
 
 app.listen(3000);
